36e25a18cd6fd05cc7455d2d3e58e21c4ea25261
mmurzin <goodlost@yandex.ru>
2019-11-20 23:56:47 +0300

    init

---
+++ HundredPushUps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HundredPushUps
+//
+//  Created by Murzin Mikhail on 20.11.2019.
+//  Copyright © 2019 Murzin Mikhail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
